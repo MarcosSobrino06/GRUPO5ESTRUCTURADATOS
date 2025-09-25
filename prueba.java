191fc7146d9f652b552bf1baf6a812773568fcbd
MarcosSobrino06 <Marcos.Sobrino1@alu.uclm.es>
2025-09-25 12:29:24 +0200

    Add files via upload

---
+++ prueba.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class prueba{
+    public static Scanner teclado = new Scanner(System.in);
+    public static void main(String[]args){
+        System.out.println("Escribe algo.");
+        String m = teclado.nextLine();
+        System.out.print(m+"\nHola mundo.");
+    }
+}
